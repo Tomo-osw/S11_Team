edf844911ba6b4b64175c32207f4a1d52308ea25
Tomo-osw <84332899+Tomo-osw@users.noreply.github.com>
2021-09-28 14:21:03 +0900

    Delete server.py

--- server.py
+++
@@ -1,9 +0,0 @@
-import os, flask
-PORT = int(os.environ['PORT'])
-app = flask.Flask('app server')
-
-@app.route("/")
-def index():
-  return 'Hello, DockerFile'
-
-app.run(debug=True, host='0.0.0.0', port=80)

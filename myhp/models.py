4752054852b361f96c6d365de754bb32bc43c659
Tomo-osw <84332899+Tomo-osw@users.noreply.github.com>
2021-10-16 13:45:36 +0900

    Update models.py

--- myhp/models.py
+++ myhp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 #ユーザー認証
 from django.contrib.auth.models import User
 
@@ -10,3 +11,29 @@ class Account(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class Lists(models.Model):
+    list_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    list_title = models.TextField
+    list_discription = models.TextField(blank = True, null = True)
+    or_list_public = models.BooleanField(default='False')
+    or_list_share = models.BooleanField(default='False')
+
+class Posts(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    list_id = models.ForeignKey('myhp.lists', on_delete=models.CASCADE)
+    user_id = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    latitude = models.TextField
+    longitude = models.TextField
+    place_name = models.TextField
+    post_title = models.TextField
+    post_discription = models.TextField(blank = True, null = True)
+    post_image = models.ImageField(upload_to='images',blank=True, null=True)
+    post_link = models.TextField(blank = True, null = True)
+    post_date = models.TextField(blank = True, null = True)
+    post_hashtag = models.TextField(blank = True, null = True)
+    or_datail = models.BooleanField(default='False')
+    or_post_share = models.BooleanField(default='False')
+    updated_time = models.DateTimeField(default=timezone.now)
+    created_time = models.DateTimeField(default=timezone.now)

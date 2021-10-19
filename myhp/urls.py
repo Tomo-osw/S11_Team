492c24790a1d0a2580dfa0190a2ce54aea0100d5
sk8hiroto <80390098+sk8hiroto@users.noreply.github.com>
2021-10-19 16:24:52 +0900

    Update urls.py

--- myhp/urls.py
+++ myhp/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from . import views
+from .views import HomeListView
 
 app_name = 'myhp'
 urlpatterns = [
     path('', views.Login, name='Login'),
     path('logout/', views.Logout, name='Logout'),
     path('register/', views.AccountRegistration.as_view(), name='register'),
-    path('home/', views.home, name='home'),
+    path('home/', HomeListView.as_view(), name='home'),
     path('makelist/', views.makelist, name='makelist'),
-    path('makepost/', views.makepost, name='makepost')
+    path('makepost/', views.makepost, name='makepost'),
+    path('editlist/', views.editlist, name='editlist'),
 ]
